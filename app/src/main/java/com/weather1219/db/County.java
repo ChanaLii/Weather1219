e3120647af669e5d9cb8f8f5836bc75016373350
ChanaLi <316529035@qq.com>
2016-12-29 22:19:46 +0800

    First commit.

---
+++ app/src/main/java/com/weather1219/db/County.java
@@ -0,0 +1,51 @@
+package com.weather1219.db;
+
+import org.litepal.crud.DataSupport;
+
+/**
+ * Created by Administrator on 2016/12/29.
+ */
+
+public class County extends DataSupport {
+
+    private int id;
+
+    private String countyName;
+
+    private String weatherId;
+
+    private int cityId;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getCountyName() {
+        return countyName;
+    }
+
+    public void setCountyName(String countyName) {
+        this.countyName = countyName;
+    }
+
+    public String getWeatherId() {
+        return weatherId;
+    }
+
+    public void setWeatherId(String weatherId) {
+        this.weatherId = weatherId;
+    }
+
+    public int getCityId() {
+        return cityId;
+    }
+
+    public void setCityId(int cityId) {
+        this.cityId = cityId;
+    }
+
+}
